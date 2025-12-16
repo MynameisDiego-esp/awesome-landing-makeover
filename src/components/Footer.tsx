9f0dfc99c4cd40fa44b4391501ae1f1730eead1d
MynameisDiego-esp <diegocgd.21@gmail.com>
2025-12-15 16:42:56 -0800

    ultimo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,8 +31,8 @@ const Footer = () => {
           <div>
             <h4 className="font-bold text-lg mb-4">Horario</h4>
             <div className="space-y-2 text-background/80">
-              <p>Lunes - Viernes: 8:00 AM - 6:00 PM</p>
-              <p>Sábado: 9:00 AM - 5:00 PM</p>
+              <p>Lunes - Viernes: 9:00 AM - 5:00 PM</p>
+              <p>Sábado: 8:00 AM - 4:00 PM</p>
               <p>Domingo: Cerrado</p>
             </div>
           </div>
